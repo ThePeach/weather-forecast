2b976f45e4d09610b3a651ac471c3509bd5fb7af
Matteo 'Peach' Pescarin <peach@smartart.it>
2017-03-08 18:09:46 +0000

    added forecast parsing module
    - updated index with relevant modules
    - updaed main script with basic data update

---
+++ src/js/forecast.js
@@ -0,0 +1,84 @@
+// This specific module deals with the data that is received by Open Weather Map
+
+const FORECAST = (function () {
+  const days = []
+  function Day () {
+    this.date = null // unix timestamp
+    this.temp = {
+      max: null,
+      min: null
+    }
+    this.wind = {
+      speed: null,
+      direction: {
+        degrees: null,
+        abbr: null
+      }
+    }
+    this.cloudCoverage = {
+      avg: function () {
+        let total = 0
+        for (let i = 0; i < this.cloudCoverage.timed.length; i++) {
+          total += this.cloudCoverage.timed[i]
+        }
+        return total / this.cloudCoverage.timed.length
+      },
+      timed: []
+    }
+    this.precipitation = {
+      avg: function () {
+        let total = 0
+        for (let i = 0; i < this.precipitation.timed.length; i++) {
+          total += this.precipitation.timed[i]
+        }
+        return total / this.precipitation.timed.length
+      },
+      timed: []
+    }
+  }
+  Day.prototype.init = function init (data) {
+    this.date = new Date(data.dt * 1000)
+    this.temp.max = data.main.temp_max
+    this.temp.min = data.main.temp_min
+    this.wind.speed = data.wind.speed
+    this.wind.direction.degrees = data.wind.deg
+    this.cloudCoverage.timed.push(data.clouds.all)
+    this.precipitation.timed.push(data.rain['3h'] || 0)
+  }
+  Day.prototype.update = function update (data) {
+    if (this.temp.max < data.main.temp_max) {
+      this.temp.max = data.main.temp_max
+    }
+    if (this.temp.min > data.main.temp_min) {
+      this.temp.min = data.main.temp_min
+    }
+    this.cloudCoverage.timed.push(data.clouds.all)
+    this.precipitation.timed.push(data.rain['3h'] || 0)
+  }
+
+  function parseData (data) {
+    let prevDay = null
+
+    for (let i = 0; i < data.list.length; i++) {
+      let currDay
+      let currDate = new Date(data.list[i].dt * 1000)
+
+      if (prevDay === null || currDate.getDay() !== prevDay.date.getDay()) {
+        // new day to parse
+        currDay = new Day()
+        currDay.init(data.list[i])
+        prevDay = currDay
+        days.push(currDay)
+      } else {
+        // we just need to update what we already have
+        currDay = prevDay
+        currDay.update(data.list[i])
+      }
+    }
+    return days
+  }
+
+  return {
+    parse: parseData
+  }
+})()
---
+++ src/js/main.js
@@ -0,0 +1,38 @@
+/* global FORECAST */
+
+function init (data) {
+  const days = FORECAST.parse(data)
+  console.log(days)
+  const dayEls = document.getElementsByClassName('w-forecast_day')
+
+  for (let i = 0; i < days.length; i++) {
+    injectData(dayEls[i], days[i])
+  }
+}
+
+function injectData (element, data) {
+  const weekDaysAbbr = ['Sun', 'Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat']
+
+  if (element === undefined) {
+    return false
+  }
+
+  let WeekDay = element.querySelector('.w-forecast_day-text')
+  WeekDay.innerHTML = weekDaysAbbr[data.date.getDay()]
+
+  let TempMax = element.querySelector('.w-forecast_temp--max')
+  TempMax.innerHTML = `${parseInt(data.temp.max, 10)}&deg;C`
+
+  let TempMin = element.querySelector('.w-forecast_temp--min')
+  TempMin.innerHTML = `${parseInt(data.temp.min, 10)}&deg;C`
+}
+
+(function () {
+  const APPID = '8db2a7582a49564a54b1336c222fbeb8'
+  const city = 'London,GB'
+  const units = 'metric'
+
+  const script = document.createElement('script')
+  script.src = `http://api.openweathermap.org/data/2.5/forecast?q=${city}&units=${units}&APPID=${APPID}&callback=init`
+  document.head.appendChild(script)
+})()
